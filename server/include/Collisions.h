ca2be81cae03d8ce897056c9334bff8ec6c30f96
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2020-12-28 01:51:30 +0300

    Коллизии пули с игроком

--- server/include/Collisions.h
+++ server/include/Collisions.h
@@ -4,27 +4,22 @@
 #include <vector>
 #include <unordered_map>
 #include <Object.h>
+#include <cmath>
 
 class CollisionManager {
 public:
     CollisionManager() = default;
-    std::vector<std::shared_ptr<Object>>check(const std::unordered_map<int, std::shared_ptr<Object>>& objects,
+    void check(const std::unordered_map<int, std::shared_ptr<Object>>& objects,
                                                                 std::shared_ptr<Object> object);
 
     bool is_object_collided(const std::unordered_map<int, std::shared_ptr<Object>>& objects,
                             std::shared_ptr<Object> object) const;
 
-    void resolve_collision(std::shared_ptr<Object> lhs_obj, std::shared_ptr<Object> rhs_obj);
-private:
-
-    bool collides(std::shared_ptr<Object> lhs_obj, std::shared_ptr<Object> rhs_obj) const ;
-
-    void resolve_collision(std::shared_ptr<Player> player, std::shared_ptr<Obstruction> obstruct);
     void resolve_collision(std::shared_ptr<Player> player, std::shared_ptr<Bullet> bullet);
-    void resolve_collision(std::shared_ptr<Bullet> bullet, std::shared_ptr<Obstruction> obstruct);
-    void resolve_collision(std::shared_ptr<Obstruction> obstruct, std::shared_ptr<Player> player);
-    void resolve_collision(std::shared_ptr<Bullet> bullet, std::shared_ptr<Player> player);
-    void resolve_collision(std::shared_ptr<Obstruction> obstruct, std::shared_ptr<Bullet> bullet);
+private:
+    static int COLLISION_RADIUS = 60;
+    bool collides(std::shared_ptr<Player> player, std::shared_ptr<Bullet> bullet) const ;
+
 
 };
 
--- server/src/Collisions.cpp
+++ server/src/Collisions.cpp
@@ -1,19 +1,35 @@
 #include "Collisions.h"
 
-std::vector<std::shared_ptr<Object>>
-CollisionManager::check(const std::unordered_map<int, std::shared_ptr<Object>> &objects,
+void CollisionManager::check(const std::unordered_map<int, std::shared_ptr<Object>> &objects,
                         std::shared_ptr<Object> object) {
+
     std::vector<std::shared_ptr<Object>> result;
-    for (auto& obj: objects) {
-        if (collides(obj.second, object)) {
-            if (obj.second->ID != object->ID) {
-                result.push_back(obj.second);
+
+    auto bullet = std::static_pointer_cast<Bullet>(object);
+
+    if(bullet->state == 0)
+        return; // неактивные пули не участвуют в коллизиях
+
+    for (auto &obj: objects) {
+        if (obj.second->type == Object::PLAYER) {
+            auto player = std::static_pointer_cast<Player>(obj.second);
+            if (collides(player, bullet) && player->ID != bullet->iniciator_ID) {
+                std::cout << "BULLET COLLIDED\n";
+                bullet->state = 0;
+                return; //Выходим из функции т.к одновременная коллизия с двумя объектами невозможна
             }
         }
     }
-    return result;
+
+
 }
 
-bool CollisionManager::collides(std::shared_ptr<Object> lhs_obj, std::shared_ptr<Object> rhs_obj) const {
-    return false;
+//void CollisionManager::resolve_collision(std::shared_ptr<Player> player, std::shared_ptr<Bullet> bullet) {
+//    std::cout << "\n\n\n\n\n\n[COLLISION MANAGER] resolve func\n\n\n\n\n\n" << std::endl;
+//
+//}
+
+bool CollisionManager::collides(std::shared_ptr<Player> player, std::shared_ptr<Bullet> bullet) const {
+    return std::abs(player->position.x - bullet->position.x) <= 60
+            && std::abs(player->position.y - bullet->position.y) <= 60;
 }
